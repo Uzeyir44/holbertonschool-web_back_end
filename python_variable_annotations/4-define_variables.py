eeccaeb29f91d97bbcb82569a5e7b2be2ebba20a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-20 17:38:48 +0400

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains annotations of variables with speicfied value
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
