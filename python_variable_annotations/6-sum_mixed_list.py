7e07ce499e7a741831141092ed9e49612acab18f
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-20 17:50:04 +0400

    the wrong syntax for combining data types

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,12 +5,12 @@ This module contains sum_mixed_list function
 from typing import *
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     This function that returns the sum of floats
 
     Args:
-        mxd_lst (list[float | int]): list with floats and ints
+        mxd_lst (List[Union[int, float]]): list with floats and ints
 
     Return:
         (float): the sum of the list elements
