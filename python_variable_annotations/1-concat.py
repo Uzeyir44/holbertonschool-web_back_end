e1e7ffa19ae92939b05a2c56af621b564eb3d365
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-20 17:28:36 +0400

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concotanates two strings
+
+    Args:
+        a (str): first variable
+        b (str): second variable
+
+    Return:
+        (str): the combination of two strings
+    """
+    return (a + b)
