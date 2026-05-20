a831678c2258eb3f4f7877db33d83e9bf7689ca5
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-20 18:00:24 +0400

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains to_kv function
+"""
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """
+    This function that returns the tuple with given variables
+
+    Args:
+        k (str): first argument
+        v (int/float): second argument
+    Return:
+        (tuple[str, float]): the tuple with given arguments
+    """
+    return (k, float(v**2))
