b83b799a30b06423c64e25dbd8fc3405f2aa4d9a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-30 14:56:04 +0400

    rask 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+IMported time, asyncio libraries and wait_n function
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    This function calculates the total time spent to execute the
+    coroutine function
+
+    Args:
+        n (int): the numbers of coroutines that will be started
+        max_delay (int): the limit of possible delay
+
+    Returns:
+        float: the total time spent on execution
+    """
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    elaps = time.perf_counter() - start
+    return (elaps)
