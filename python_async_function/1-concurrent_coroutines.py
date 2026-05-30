1b2eca51e7186841af17e2bfaae724f2e2e07288
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-30 14:46:02 +0400

    pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Imported asyncio, random, and typing libraries   
+Imported asyncio, random, and typing libraries
 """
 import asyncio
 from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
