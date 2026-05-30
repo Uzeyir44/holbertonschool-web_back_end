764097773a376fa1fe6eac4da6d0279b5293159f
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-30 15:17:25 +0400

    pycodestyle and readme file

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,8 +2,8 @@
 """
 Imported wait_random function
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
@@ -11,7 +11,8 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     This function wrpas coroutines into tasks
 
     Args:
-        max_delay (int): the argumet that will be passes into wait_random function
+        max_delay (int): the argumet that will be passes into
+        wait_random function
 
     Returns:
         asyncio.Task: the cocroutine wrapped into task
