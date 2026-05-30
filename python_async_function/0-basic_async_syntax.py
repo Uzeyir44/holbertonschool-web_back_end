e3759b6a1155b5113f466f0cf4efcd02f5123b54
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-30 14:08:29 +0400

    pycodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,8 @@ two modules , namely asyncio and random were imported
 import asyncio
 import random
 
-async def wait_random(max_delay: int =10) -> float:
+
+async def wait_random(max_delay: int = 10) -> float:
     """
     This function stops execution for a certain period of time
     and then returns this value
@@ -15,7 +16,7 @@ async def wait_random(max_delay: int =10) -> float:
         temporarily terminated. Defaults to 10.
 
     Returns:
-        float: the amount if time during which function was stopped 
+        float: the amount if time during which function was stopped
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
