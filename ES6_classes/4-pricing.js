00d079eba728028d29e6212511ac5b5d6ae10344
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-07-04 21:19:38 +0400

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency'
+
+export default class Price {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set amount(value) {
+        this._amount = value;
+    }
+
+    set currency(value) {
+        this._currency = value;
+    }
+
+    displayFullPrice() {
+        return `${this_amount} ${this._currency}`
+    }
+
+    convertPrice(amount, conversionRate) {
+        return amount*conversionRate;
+    }
+}
