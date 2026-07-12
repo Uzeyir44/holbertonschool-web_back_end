918b045949af07b9af06115985b25c560cb2f5b0
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-07-12 23:36:59 +0400

    another data type was needed

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
     const buff = new ArrayBuffer(length);
-    const view = new Int8Array(buff);
+    const view = new DataView(buff);
 
     if (position < 0 || position >= length) {
         throw new Error('Position outside range');
     }
 
-    view[position] = value;
+    view.setInt8(position, value);
     return view;
 }
