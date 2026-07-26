99aa0b05b182a2884c2f74190fee70e1673d6464
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-07-26 23:14:38 +0400

    errors handled

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r\n`);
+  console.log(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\r\n');
+  console.log('This important software is now closing');
 });
